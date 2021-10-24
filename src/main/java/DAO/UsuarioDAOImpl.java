7644dbcc438a98c2dae6a6763af5ab841184ad2b
Ricardo Tolomei <87551219+32514243@users.noreply.github.com>
2021-10-24 17:35:37 -0300

    Corregi insert en el Dao y agregue posicion a atracciones en sql
    le corregi los insert y le agregue posicion a la tabla de atracciones en el Sql

--- src/main/java/DAO/UsuarioDAOImpl.java
+++ src/main/java/DAO/UsuarioDAOImpl.java
@@ -15,19 +15,19 @@ public class UsuarioDAOImpl implements UsuarioDAO {
 	@Override
 	public int insert(Usuario user) {
 		try {
-			String sql = "INSERT INTO Usuario (DNI, tipoFavorito, dineroInicial, dineroDisponible, tiempoInicial, "
-					+ "tiempoDisponible, atracciones, costoTotal) VALUES (?,?,?,?,?,?,?,?)";
+			String sql = "INSERT INTO Usuario (usuario, contraseña, dinero, tiempo, costoTotal, "
+					+" posicionX, posicionY) VALUES (?,?,?,?,?,?,?,?)";
 			Connection conn = ConnectionProvider.getConnection();
 
 			PreparedStatement statement = conn.prepareStatement(sql);
 			statement.setLong(1, user.getDNI());
-			statement.setString(2, user.getTipoFavorito().toString());
-			statement.setDouble(3, user.getDineroInicial());
-			statement.setDouble(4, user.getDineroDisponible());
-			statement.setDouble(5, user.getTiempoInicial());
-			statement.setDouble(6, user.getTiempoDisponible());
-			statement.setString(7, user.getAtracciones().toString());
-			statement.setDouble(8, user.getCostoTotal());
+			statement.setString(2, user.getContraseña().toString());
+			statement.setDouble(3, user.getDineroDisponible());
+			statement.setDouble(4, user.getTiempoDisponible());
+			statement.setDouble(5, user.getCostoTotal());
+			statement.setInt(6, user.getPosicionX());
+			statement.setInt(7, user.getPosicionY());
+			
 			
 			int rows = statement.executeUpdate();
 
