a4b1ae37e976a31d5e8c6bb523504606fc270c18
GastonCP <gamafa262@gmail.com>
2021-10-25 00:48:06 -0300

    metodos de UsuariosDaoImpl.java añadidos. NO TESTEADOS.

--- src/main/java/DAO/UsuarioDAOImpl.java
+++ src/main/java/DAO/UsuarioDAOImpl.java
@@ -7,6 +7,8 @@ import java.sql.SQLException;
 import java.util.LinkedList;
 import java.util.List;
 
+import main.java.DAO.MissingDataException;
+import main.java.enumeradores.ENUMTIPO;
 import main.java.jdbc.ConnectionProvider;
 import main.java.usuarios.Usuario;
 
@@ -15,20 +17,70 @@ public class UsuarioDAOImpl implements UsuarioDAO {
 	@Override
 	public int insert(Usuario user) {
 		try {
-			String sql = "INSERT INTO Usuario (usuario, contraseña, dinero, tiempo, costoTotal, "
-					+" posicionX, posicionY) VALUES (?,?,?,?,?,?,?,?)";
+			String sql = "INSERT INTO Usuario (DNI, usuario, contraseña, dinero, tiempo, costoTotal, "
+					+ " posicionX, posicionY) VALUES (?,?,?,?,?,?,?,?)";
 			Connection conn = ConnectionProvider.getConnection();
 
 			PreparedStatement statement = conn.prepareStatement(sql);
 			statement.setLong(1, user.getDNI());
-			statement.setString(2, user.getContraseña().toString());
-			statement.setDouble(3, user.getDineroDisponible());
-			statement.setDouble(4, user.getTiempoDisponible());
-			statement.setDouble(5, user.getCostoTotal());
-			statement.setInt(6, user.getPosicionX());
-			statement.setInt(7, user.getPosicionY());
-			
-			
+			statement.setString(2, user.getUsuario().toString());
+			statement.setString(3, user.getContraseña().toString());
+			statement.setDouble(4, user.getDineroDisponible());
+			statement.setDouble(5, user.getTiempoDisponible());
+			statement.setDouble(6, user.getCostoTotal());
+			statement.setInt(7, user.getPosicionX());
+			statement.setInt(8, user.getPosicionY());
+			int rows = statement.executeUpdate();
+
+			return rows;
+		} catch (Exception e) {
+			throw new MissingDataException(e);
+		}
+	}
+
+	@Override
+	public int countAll() {
+		try {
+			String sql = "SELECT COUNT(1) AS TOTAL FROM USERS";
+			Connection conn = ConnectionProvider.getConnection();
+			PreparedStatement statement = conn.prepareStatement(sql);
+			ResultSet resultados = statement.executeQuery();
+
+			resultados.next();
+			int total = resultados.getInt("TOTAL");
+
+			return total;
+		} catch (Exception e) {
+			throw new MissingDataException(e);
+		}
+	}
+
+	// solo esta haciendo un cambio de contraseña
+	@Override
+	public int update(Usuario user) {
+		try {
+			String sql = "UPDATE USERS SET PASSWORD = ? WHERE USERNAME = ?";
+			Connection conn = ConnectionProvider.getConnection();
+
+			PreparedStatement statement = conn.prepareStatement(sql);
+			statement.setString(1, user.getContraseña().toString());
+			statement.setString(2, user.getUsuario().toString());
+			int rows = statement.executeUpdate();
+
+			return rows;
+		} catch (Exception e) {
+			throw new MissingDataException(e);
+		}
+	}
+
+	// Elimina Segun DNI
+	public int delete(Usuario user) {
+		try {
+			String sql = "DELETE FROM USERS WHERE DNI = ?";
+			Connection conn = ConnectionProvider.getConnection();
+
+			PreparedStatement statement = conn.prepareStatement(sql);
+			statement.setLong(1, user.getDNI());
 			int rows = statement.executeUpdate();
 
 			return rows;
@@ -39,32 +91,46 @@ public class UsuarioDAOImpl implements UsuarioDAO {
 
 	@Override
 	public List<Usuario> findAll() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		try {
+			String sql = "SELECT * FROM USERS";
+			Connection conn = ConnectionProvider.getConnection();
+			PreparedStatement statement = conn.prepareStatement(sql);
+			ResultSet resultados = statement.executeQuery();
 
-	@Override
-	public int countAll() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+			List<Usuario> usuarios = new LinkedList<Usuario>();
+			while (resultados.next()) {
+				usuarios.add(toUsuarios(resultados));
+			}
 
-	@Override
-	public int update(Usuario t) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public int delete(Usuario t) {
-		// TODO Auto-generated method stub
-		return 0;
+			return usuarios;
+		} catch (Exception e) {
+			throw new MissingDataException(e);
+		}
 	}
 
 	@Override
 	public Usuario findByDNI(int dni) {
-		// TODO Auto-generated method stub
-		return null;
+			try {
+				String sql = "SELECT * FROM USERS WHERE DNI = ?";
+				Connection conn = ConnectionProvider.getConnection();
+				PreparedStatement statement = conn.prepareStatement(sql);
+				statement.setInt(1, dni);
+				ResultSet resultados = statement.executeQuery();
+
+				Usuario user = null;
+
+				if (resultados.next()) {
+					user = toUsuarios(resultados);
+				}
+
+				return user;
+			} catch (Exception e) {
+				throw new MissingDataException(e);
+			}
 	}
 
+	private Usuario toUsuarios(ResultSet resultados) throws SQLException {
+		//return new Usuario(resultados.getInt(1), resultados.getString(2));
+		return new Usuario(resultados.getInt(1), null, resultados.getInt(4), resultados.getDouble(5));
+	}
 }
--- src/main/java/usuarios/Usuario.java
+++ src/main/java/usuarios/Usuario.java
@@ -1,61 +1,61 @@
 package main.java.usuarios;
 
- import main.java.sugeribles.Sugerencia;
- import main.java.sugeribles.Atraccion;
- import main.java.enumeradores.ENUMTIPO;
+import main.java.sugeribles.Sugerencia;
+import main.java.sugeribles.Atraccion;
+import main.java.enumeradores.ENUMTIPO;
 
- import java.util.*;
+import java.util.*;
 
 public class Usuario {
-    private final int DNI;
-    private final ENUMTIPO tipoFavorito;
-    
-    //combinar
-    private final int dineroInicial;
-    private int dineroDisponible;
-    
-    //combinar
-    private final double tiempoInicial;
-    private double tiempoDisponible;
-    
-    private final List<Atraccion> atracciones;
-    private int costoTotal;	//??
+	private final int DNI;
+	private ENUMTIPO tipoFavorito;
 
-	//Nuevos añadidos
-    private String usuario;
-    private String contraseña;
-    private int posicionX;
-    private int posicionY;
+	// combinar
+	private final int dineroInicial;
+	private int dineroDisponible;
 
-    public Usuario(int DNI, ENUMTIPO tipoFavorito, int dineroInicial, double tiempoDisponible) {
-        this.DNI = DNI;
-        this.tipoFavorito = tipoFavorito;
-        this.dineroInicial = dineroInicial;
-        this.dineroDisponible = dineroInicial;
-        this.tiempoInicial = tiempoDisponible;
-        this.tiempoDisponible = tiempoDisponible;
-        costoTotal = 0;
-        atracciones = new ArrayList<>();
-    }
+	// combinar
+	private final double tiempoInicial;
+	private double tiempoDisponible;
 
-    public int getDNI() {
-        return DNI;
-    }
+	private final List<Atraccion> atracciones;
+	private int costoTotal; // ??
 
-    public ENUMTIPO getTipoFavorito() {
-        return tipoFavorito;
-    }
+	// Nuevos añadidos
+	private String usuario;
+	private String contraseña;
+	private int posicionX;
+	private int posicionY;
 
-    public int getDineroDisponible() {
-        return dineroDisponible;
-    }
+	public Usuario(int DNI, ENUMTIPO tipoFavorito, int dineroInicial, double tiempoDisponible) {
+		this.DNI = DNI;
+		this.tipoFavorito = tipoFavorito;
+		this.dineroInicial = dineroInicial;
+		this.dineroDisponible = dineroInicial;
+		this.tiempoInicial = tiempoDisponible;
+		this.tiempoDisponible = tiempoDisponible;
+		costoTotal = 0;
+		atracciones = new ArrayList<>();
+	}
 
-    public double getTiempoDisponible() {
-        return tiempoDisponible;
-    }
-    
-    //Nuevos añadidos
-    public String getUsuario() {
+	public int getDNI() {
+		return DNI;
+	}
+
+	public ENUMTIPO getTipoFavorito() {
+		return tipoFavorito;
+	}
+
+	public int getDineroDisponible() {
+		return dineroDisponible;
+	}
+
+	public double getTiempoDisponible() {
+		return tiempoDisponible;
+	}
+
+	// Nuevos añadidos //
+	public String getUsuario() {
 		return usuario;
 	}
 
@@ -86,82 +86,91 @@ public class Usuario {
 	public void setPosicionY(int posicionY) {
 		this.posicionY = posicionY;
 	}
-	//fin Nuevos añadididos
 
-    @Override
-    public String toString() {
-        return "Usuario" + "tipoFavorito=" + tipoFavorito + ", presupuesto=" + dineroDisponible + ", tiempoDisponible=" + tiempoDisponible + '}';
-    }
+	public void setTipoFavorito(ENUMTIPO tipoFavorito) {
+		this.tipoFavorito = tipoFavorito;
+	}
 
-    public void recibirSugerencia(Sugerencia sugerencia) {
-        System.out.println("\n\nSe ha hecho la siguiente sugerencia:\n");
-        if (sugerencia.getPromocion() == null){
-            System.out.println("La atraccion:\n" + sugerencia.getAtracciones()[0]
-                    + "\nTotal a pagar: " + sugerencia.getTotal() + " monedas");
-        }else{
-            StringBuilder concatenado = new StringBuilder();
-            for (Atraccion atraccion : sugerencia.getAtracciones()) {
-                concatenado.append(atraccion).append("\n");
-            }
-            System.out.println("La promocion es de tipo: " + sugerencia.getPromocion().getClass().toString().split("\\.")[2]
-                    + "\nLas atracciones:\n" + concatenado
-                    + "\nTotal a pagar: " + sugerencia.getTotal() + " monedas."
-                    + "\nSe ahorra " + calcularDifereciaMonedas(sugerencia) + " monedas.");
-        }
-        String aceptar;
-        do {
-            System.out.println("\nÂ¿Desea aceptar la sugerencia?\nIngrese \"Si\" para aceptar, de lo contrario ingrese \"No\" para rechazarlo.");
-            aceptar = ingresarDatoStr();
-            //aceptar = in.nextLine();
-        }while (!aceptar.equalsIgnoreCase("Si") && !aceptar.equalsIgnoreCase("No"));
-        if (aceptar.equalsIgnoreCase("Si")){
-            dineroDisponible -= sugerencia.getTotal();
-            for (Atraccion atraccion : sugerencia.getAtracciones()) {
-                tiempoDisponible -= atraccion.getTiempo();
-                atraccion.ocuparUnLugar();
-            }
-            costoTotal += sugerencia.getTotal();
-            Collections.addAll(atracciones, sugerencia.getAtracciones());
-        }
-    }
+	// fin Nuevos añadididos //
 
-    private int calcularDifereciaMonedas(Sugerencia sugerencia){
-        int valorOriginal = 0;
-        for (int i = 0; i < sugerencia.getAtracciones().length; i++) {
-            valorOriginal += sugerencia.getAtracciones()[i].getCosto();
-        }
-        return valorOriginal - sugerencia.getTotal();
-    }
+	@Override
+	public String toString() {
+		return "Usuario" + "tipoFavorito=" + tipoFavorito + ", presupuesto=" + dineroDisponible + ", tiempoDisponible="
+				+ tiempoDisponible + '}';
+	}
 
-    public List<Atraccion> getAtracciones() {
-        return atracciones;
-    }
+	public void recibirSugerencia(Sugerencia sugerencia) {
+		System.out.println("\n\nSe ha hecho la siguiente sugerencia:\n");
+		if (sugerencia.getPromocion() == null) {
+			System.out.println("La atraccion:\n" + sugerencia.getAtracciones()[0] + "\nTotal a pagar: "
+					+ sugerencia.getTotal() + " monedas");
+		} else {
+			StringBuilder concatenado = new StringBuilder();
+			for (Atraccion atraccion : sugerencia.getAtracciones()) {
+				concatenado.append(atraccion).append("\n");
+			}
+			System.out.println(
+					"La promocion es de tipo: " + sugerencia.getPromocion().getClass().toString().split("\\.")[2]
+							+ "\nLas atracciones:\n" + concatenado + "\nTotal a pagar: " + sugerencia.getTotal()
+							+ " monedas." + "\nSe ahorra " + calcularDifereciaMonedas(sugerencia) + " monedas.");
+		}
+		String aceptar;
+		do {
+			System.out.println(
+					"\nÂ¿Desea aceptar la sugerencia?\nIngrese \"Si\" para aceptar, de lo contrario ingrese \"No\" para rechazarlo.");
+			aceptar = ingresarDatoStr();
+			// aceptar = in.nextLine();
+		} while (!aceptar.equalsIgnoreCase("Si") && !aceptar.equalsIgnoreCase("No"));
+		if (aceptar.equalsIgnoreCase("Si")) {
+			dineroDisponible -= sugerencia.getTotal();
+			for (Atraccion atraccion : sugerencia.getAtracciones()) {
+				tiempoDisponible -= atraccion.getTiempo();
+				atraccion.ocuparUnLugar();
+			}
+			costoTotal += sugerencia.getTotal();
+			Collections.addAll(atracciones, sugerencia.getAtracciones());
+		}
+	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Usuario usuario = (Usuario) o;
-        return DNI == usuario.DNI;
-    }
+	private int calcularDifereciaMonedas(Sugerencia sugerencia) {
+		int valorOriginal = 0;
+		for (int i = 0; i < sugerencia.getAtracciones().length; i++) {
+			valorOriginal += sugerencia.getAtracciones()[i].getCosto();
+		}
+		return valorOriginal - sugerencia.getTotal();
+	}
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(DNI);
-    }
+	public List<Atraccion> getAtracciones() {
+		return atracciones;
+	}
 
-    public int getCostoTotal() {
-        return costoTotal;
-    }
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		Usuario usuario = (Usuario) o;
+		return DNI == usuario.DNI;
+	}
 
-    public int getDineroInicial() {
-        return dineroInicial;
-    }
+	@Override
+	public int hashCode() {
+		return Objects.hash(DNI);
+	}
 
-    public double getTiempoInicial() {
-        return tiempoInicial;
-    }
-    
+	public int getCostoTotal() {
+		return costoTotal;
+	}
+
+	public int getDineroInicial() {
+		return dineroInicial;
+	}
+
+	public double getTiempoInicial() {
+		return tiempoInicial;
+	}
+
 	private static String ingresarDatoStr() {
 		@SuppressWarnings("resource")
 		Scanner scan = new Scanner(System.in);
