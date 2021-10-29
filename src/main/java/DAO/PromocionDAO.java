f141779a7424911213fb03bca0bfc7cb2e9b49fe
Gabriel Pereyra <gabrielp1689@gmail.com>
2021-10-29 19:21:19 -0300

    se adiciona DAO para promociones (incompletos)

--- src/main/java/DAO/PromocionDAO.java
+++ src/main/java/DAO/PromocionDAO.java
@@ -1,13 +1,8 @@
 package main.java.DAO;
-import main.java.sugeribles.Atraccion;
-import main.java.sugeribles.promociones.*;
 
+import main.java.sugeribles.promociones.Promocion;
 
 public interface PromocionDAO extends GenericDAO<Promocion>{
-	
 	public abstract Promocion findByID (Integer id);
-	
+
 }
-
-
-
--- src/main/java/DAO/PromocionDAOImpl.java
+++ src/main/java/DAO/PromocionDAOImpl.java
@@ -6,59 +6,50 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.LinkedList;
 import java.util.List;
-
 import main.java.jdbc.ConnectionProvider;
+import main.java.sugeribles.Atraccion;
 import main.java.sugeribles.promociones.Promocion;
-import main.java.usuarios.Usuario;
 
 public class PromocionDAOImpl implements PromocionDAO{
-	@Override
-	public int insert(Promocion t) {
-		// TODO Auto-generated method stub
-		
-		
-		
-		
-		return 0;
-	}
 
 	@Override
-	public int update(Promocion prom) {
-		/*try {
-			String sql = "UPDATE Promotions SET type = ?, numbreIdAttr1 = ?,"
-					+ " numbreIdAttr2 = ?, numbreIdAttrPlus = ?, porcentageDisc = ?,totalCost=? WHERE id = ?";
+	public int insert(Promocion promocion) {
+		ry {
+			String sql = "INSERT INTO Attractions (attractionName, costInCoins, timeHs, capacityMax, capacity, positionX, positionY) "
+					+ "VALUES (?,?,?,?,?,?)";
 			Connection conn = ConnectionProvider.getConnection();
 
 			PreparedStatement statement = conn.prepareStatement(sql);
-			statement.setInt(1, prom.getTipo());
-			statement.setAtraccion(2, prom.getAtraccionA());
-			statement.setDouble(3, prom.getTiempo());
-			statement.setInt(4, prom.getAtraP());
-			statement.setInt(5, prom.getPorcentaje());
-			statement.setString(6, prom.getTotal());
-			int rows = statement.executeUpdate();
-
-			return rows;
-		} catch (Exception e) {
-			throw new MissingDataException(e);
-		}*/
-		return 0;
-	}
-
-	@Override
-	public int delete(Promocion prom) {
-		try {
-			String sql = "DELETE FROM Promotions WHERE id = ?";
-			Connection conn = ConnectionProvider.getConnection();
-
-			PreparedStatement statement = conn.prepareStatement(sql);
-			statement.setInt(1, prom.getId());
+			statement.setInt(1, promocion.);
+			statement.setDouble(2, promocion.getCosto());
+			statement.setDouble(3, promocion.getTiempo());
+			statement.setInt(4, promocion.getCupo());
+			statement.setInt(5, promocion.getPuestosOcupados());
 			int rows = statement.executeUpdate();
 
 			return rows;
 		} catch (Exception e) {
 			throw new MissingDataException(e);
 		}
+	}
+	/*
+	"type"	INTEGER NOT NULL DEFAULT 01,
+	"numbreIdAttr1"	INTEGER NOT NULL DEFAULT 01,
+	"numbreIdAttr2"	INTEGER NOT NULL DEFAULT 02,
+	"numbreIdAttrPlus"	INTEGER,
+	"porcentageDisc"	REAL,
+	"totalCost"	REAL,*/
+
+	@Override
+	public int update(Promocion t) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int delete(Promocion t) {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 
 	@Override
@@ -68,46 +59,15 @@ public class PromocionDAOImpl implements PromocionDAO{
 	}
 
 	@Override
-	public List<Promocion> findAll() {	
-		try {
-			String sql = "SELECT * FROM Promotions";
-			Connection conn = ConnectionProvider.getConnection();
-			PreparedStatement statement = conn.prepareStatement(sql);
-			ResultSet resultados = statement.executeQuery();
-
-			List<Promocion> promotions = new LinkedList<Promocion>();
-			while (resultados.next()) {
-				promotions.add(toPromciones(resultados));
-			}
-
-			return promotions;
-			} catch (Exception e) {
-			throw new MissingDataException(e);
-			}
-		}
-
-	private Promocion toPromciones(ResultSet resultados)throws SQLException {
-		return new Promocion(resultados.getInt(1), resultados.getInt(2), resultados.getInt(3),
-				resultados.getInt(4), resultados.getInt(5), resultados.getString(6), resultados.getString(7));
+	public List<Promocion> findAll() {
+		// TODO Auto-generated method stub
+		return null;
 	}
-
-	
 
 	@Override
 	public int countAll() {
-		try {
-			String sql = "SELECT COUNT(1) AS TOTAL FROM Promotions";
-			Connection conn = ConnectionProvider.getConnection();
-			PreparedStatement statement = conn.prepareStatement(sql);
-			ResultSet resultados = statement.executeQuery();
-
-			resultados.next();
-			int total = resultados.getInt("TOTAL");
-
-			return total;
-		} catch (Exception e) {
-			throw new MissingDataException(e);
-		}
+		// TODO Auto-generated method stub
+		return 0;
 	}
 
 	@Override
@@ -115,5 +75,5 @@ public class PromocionDAOImpl implements PromocionDAO{
 		// TODO Auto-generated method stub
 		return null;
 	}
-}
 	
+}
